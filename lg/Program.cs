fc4e109397e5db002276b23598a9ba4394aabbb3
kevin <chengzihu@hotmail.com>
2016-03-11 14:27:50 +0800

    add

---
+++ lg/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process myProcess = new Process();
+
+            string fileName = @"C:\Users\admin\Desktop\lg\WCS\WCS20160112\WCS.exe";
+
+            string para = @"你好 北京欢迎你！";
+
+            ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(fileName, para);
+
+            myProcess.StartInfo = myProcessStartInfo;
+
+            myProcess.Start();
+
+            while (!myProcess.HasExited)
+            {
+
+                myProcess.WaitForExit();
+
+            }
+
+            int returnValue = myProcess.ExitCode;
+
+            Console.Read();
+        }
+    }
+}
